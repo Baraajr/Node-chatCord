4a6d2523f314df3c056cf22afb1ab5455aef9806
Ahmed baraa <ahmedbaraa009@gmail.com>
2025-10-30 21:46:23 +0300

    final commit

--- utils/users.js
+++ utils/users.js
@@ -1,27 +1,25 @@
-const users = [];
+const users = []
 
 //user join to chat
-exports.userJoin = function (id, username, room) {
-  const user = { id, username, room };
+exports.userJoin =  function(id, username, room){
+    const user = {id, username, room}
 
-  users.push(user);
-  return user;
-};
+    users.push(user)
+    return user
+}
 
-exports.getCurrentUser = function (id) {
-  return users.find((user) => (user.id = id));
-};
+exports. getCurrentUser = function (id){
+    return users.find(user => user.id = id)
+}
 
 // user leaves the chat
-exports.userLeave = function (id) {
-  const index = users.findIndex((user) => user.id === id);
+exports.userLeave = function(id){
+    const index = users.findIndex(user => user.id === id)
 
-  if (index !== -1) {
-    const result = users.splice(index, 1)[0];
-    console.log(result);
-    return result;
-  }
-};
+    if(index !== -1){
+        return users.splice(index,1)[0]
+    }
+}
 
 // get room users
-exports.getRoomUsers = (room) => users.filter((user) => (user.room = room));
+exports.getRoomUsers = (room) => users.filter(user => user.room = room)
